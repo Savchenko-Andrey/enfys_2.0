ab3ae8abf9da83fe28e3678cd4ab5b0483d88f56
VolodymyrP <Povzun.volodymyr@gmail.com>
2025-03-07 22:24:03 +0100

    static pages added from footer

---
+++ src/app/dostavka-ta-oplata/page.jsx
@@ -0,0 +1,11 @@
+import About from "@/libs/components/about/about";
+import Hero from "@/libs/components/hero/Hero";
+
+export default async function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  );
+}
